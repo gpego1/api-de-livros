85d1bf3720c87b35ec5cc75e8f17a56dc7825dde
gpego1 <gpego786@gmail.com>
2025-07-27 20:04:58 -0300

    Implementando JWT

--- src/main/java/io/github/cursospringboot/libraryapi/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/cursospringboot/libraryapi/config/AuthorizationServerConfig.java
@@ -1,4 +1,9 @@
 package io.github.cursospringboot.libraryapi.config;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -7,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -15,7 +21,12 @@ import org.springframework.security.oauth2.server.authorization.settings.TokenSe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -59,5 +70,35 @@ public class AuthorizationServerConfig {
         return ClientSettings.builder()
                 .requireAuthorizationConsent(false)
                 .build();
+    }
+
+    // JWK - JSON WEB KEY
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() throws Exception{
+        RSAKey rsaKey = generateKeyRSA();
+        JWKSet jwkSet = new JWKSet();
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    // METODO AUXILIAR PARA GERAR CHAVES RSA PRIVADA OU PUBLICA P/AUTENTIFICACAO DE CLIENT
+    private RSAKey generateKeyRSA() throws Exception{
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keypair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) keypair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keypair.getPrivate();
+
+        return new RSAKey
+                .Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource){
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
